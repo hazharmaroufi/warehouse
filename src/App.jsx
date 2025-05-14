8e629c009bd9c6d405cbccab07ff988d3558a604
Hazhar Maroufi <hazhmar@gmail.com>
2025-05-14 19:22:32 +0330

    ba movafagiyat az server data ro gereftam

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([]);
+  const queryKey = ["getItems"];
+  const queryFn = () => {
+    axios
+      .get("http://localhost:3000/products?page=1&limit=10")
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  const { isPending, data } = useQuery({
+    queryKey,
+    queryFn,
+  });
+  if (isPending) return "Loading...";
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {items?.data?.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
